b49e50fcb3a22d103509b9bc54d1fd75801f62d0
huhuang03 <huhuang03@gmail.com>
2019-07-07 09:10:48 +0800

    Initial

---
+++ modinfo.lua
@@ -0,0 +1,6 @@
+name="th_easy_game"
+version="1.0"
+author="th"
+description="for easy game"
+API_VERSION = 6
+dont_starve_compatible=true
---
+++ modmain.lua
@@ -0,0 +1,8 @@
+local function make_map_global_see()
+end
+
+local function quick_
+
+make_map_global_see()
+
+print("Hello in modmain")
